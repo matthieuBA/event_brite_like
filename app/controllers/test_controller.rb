5f599aa23df76afb387fa019b679b4c09c09e308
matthieuBA <matthieubacheley@gmail.com>
2020-05-11 16:22:38 +0200

    gmail

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,6 @@
 class TestController < ApplicationController
   def index
-      # user=User.create(name:"Fefe", email: "fefe39000@yopmail.com")
-      # UserMailer.welcome_email(user).deliver_now
+      user=User.create(name:"Fefe", email: "fefe39000@yopmail.com")
+      UserMailer.welcome_email(user).deliver_now
   end
 end
